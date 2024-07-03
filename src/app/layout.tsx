b7d8b0b56a7709b33ce1bf987acd06ce303debcb
youssef-lahdhiri <lahdhiriyoussef0@gmail.com>
2024-07-03 16:56:44 +0100

    fixed clerk publishable key not found during build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}>
       <html lang="en" suppressHydrationWarning>
         <body className={cn(font.className, "dark:bg-[#313338]")}>
           <ThemeProvider
