c7967a8eedad5467bd75fa18d403bd891b10163c
youssef-lahdhiri <lahdhiriyoussef0@gmail.com>
2024-07-03 16:49:57 +0100

    fixed header problem while deploying

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -4,12 +4,12 @@ import { Message } from "@prisma/client";
 import { NextResponse } from "next/server";
     const MESSAGES_BATCH=10
 export async function GET(req:Request){
-
-    try{
-        const profil=await currentProfil()
         const {searchParams}=new URL(req.url)
         const cursor=searchParams.get("cursor")
         const channelId=searchParams.get("channelId")
+    try{
+        const profil=await currentProfil()
+       
         if(!profil){
             return new NextResponse("unauthorized",{status:401})
         }
